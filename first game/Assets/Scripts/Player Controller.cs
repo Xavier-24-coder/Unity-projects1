42def7ca44b0c9e64b0465df55ccb59061fd5a53
Xavier-24-coder <nelsonxav06@gmail.com>
2025-09-12 10:23:39 -0700

    camera neraly compleate and healing things

--- first game/Assets/Scripts/Player Controller.cs
+++ first game/Assets/Scripts/Player Controller.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     Vector2 cameraRotatation;
     Vector3 cameraOffset;
+    Transform playerCam;
+
     InputAction lookVector;
-    Camera playerCam;
+
     Rigidbody rb;
 
     float verticalMove;
@@ -18,27 +21,43 @@ public class PlayerController : MonoBehaviour
     public float Ysensitivity = 1.0f;
     public float camRotationLimit = 90.0f;
 
+    public int health = 100;
+    public int maxHealth = 100;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Start()
     {
-        cameraOffset = new Vector3(0, .5f, -.5f);
+        cameraOffset = new Vector3(0, .5f, .5f);
         rb = GetComponent<Rigidbody>();
-        playerCam = Camera.main;
+        playerCam = transform.GetChild(0);
         lookVector = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
         cameraRotatation = Vector2.zero;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
         //Camera Rotation System
-        playerCam.transform.position = transform.position + cameraOffset;
+
+
         cameraRotatation.x += lookVector.ReadValue<Vector2>().x * Xsensitivity;
         cameraRotatation.y += lookVector.ReadValue<Vector2>().y * Ysensitivity;
 
         cameraRotatation.y = Mathf.Clamp(cameraRotatation.y, -camRotationLimit, camRotationLimit);
 
-        playerCam.transform.rotation = Quaternion.Euler(-cameraRotatation.y, cameraRotatation.x, 0);
+        playerCam.rotation = Quaternion.Euler(-cameraRotatation.y, cameraRotatation.x, 0);
         transform.localRotation = Quaternion.AngleAxis(cameraRotatation.x, Vector3.up);
 
         //Movement system
@@ -56,5 +75,29 @@ public class PlayerController : MonoBehaviour
 
         verticalMove = inputAxis.y;
         horizontalMove = inputAxis.x;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "killzone")
+            health = 0;
+
+        if (health < maxHealth)
+        {
+
+            if (other.tag == "healLV1")
+            { health += 10;
+                Destroy(other.gameObject);
+            }
+        }
+
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "healzone")
+        {
+            health += 1;
+        }
     }
 }
